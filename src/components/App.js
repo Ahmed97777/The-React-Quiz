53e037749b689fc07e00a59a47bb514034a871ba
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-09-04 09:02:13 +0300

    added structure, started the Quiz section.

---
+++ src/components/App.js
@@ -0,0 +1,70 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+
+function reducer(currState, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...currState, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...currState, status: "error" };
+    case "start":
+      return { ...currState, status: "active" };
+    case "newAnswer":
+      return { ...currState, answer: action.payload };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
+function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  // console.log("1", questions, "2", status);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
